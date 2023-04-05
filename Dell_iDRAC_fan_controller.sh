bf1640a1f70abfed3682b716ff67830f61fed755
Rabbslab <129987558+Rabbslab@users.noreply.github.com>
2023-04-05 21:46:22 +0100

    Update Dell_iDRAC_fan_controller.sh

--- Dell_iDRAC_fan_controller.sh
+++ Dell_iDRAC_fan_controller.sh
@@ -6,25 +6,24 @@
 
 # Define global functions
 # This function applies Dell's default dynamic fan control profile
-function apply_Dell_fan_control_profile () {
+function apply_Dell_fan_control_profile() {
   # Use ipmitool to send the raw command to set fan control to Dell default
-  ipmitool -I $IDRAC_LOGIN_STRING raw 0x30 0x30 0x01 0x01 > /dev/null
+  ipmitool -I $IDRAC_LOGIN_STRING raw 0x30 0x30 0x01 0x01 >/dev/null
   CURRENT_FAN_CONTROL_PROFILE="Dell default dynamic fan control profile"
 }
 
 # This function applies a user-specified static fan control profile
-function apply_user_fan_control_profile () {
+function apply_user_fan_control_profile() {
   # Use ipmitool to send the raw command to set fan control to user-specified value
-  ipmitool -I $IDRAC_LOGIN_STRING raw 0x30 0x30 0x01 0x00 > /dev/null
-  ipmitool -I $IDRAC_LOGIN_STRING raw 0x30 0x30 0x02 0xff $HEXADECIMAL_FAN_SPEED > /dev/null
+  ipmitool -I $IDRAC_LOGIN_STRING raw 0x30 0x30 0x01 0x00 >/dev/null
+  ipmitool -I $IDRAC_LOGIN_STRING raw 0x30 0x30 0x02 0xff $HEXADECIMAL_FAN_SPEED >/dev/null
   CURRENT_FAN_CONTROL_PROFILE="User static fan control profile ($DECIMAL_FAN_SPEED%)"
 }
 
 # Retrieve temperature sensors data using ipmitool
 # Usage : retrieve_temperatures $IS_EXHAUST_TEMPERATURE_SENSOR_PRESENT $IS_CPU2_TEMPERATURE_SENSOR_PRESENT
-function retrieve_temperatures () {
-  if (( $# != 2 ))
-  then
+function retrieve_temperatures() {
+  if (($# != 2)); then
     printf "Illegal number of parameters.\nUsage: retrieve_temperatures \$IS_EXHAUST_TEMPERATURE_SENSOR_PRESENT \$IS_CPU2_TEMPERATURE_SENSOR_PRESENT" >&2
     return 1
   fi
@@ -36,8 +35,7 @@ function retrieve_temperatures () {
   # Parse CPU data
   local CPU_DATA=$(echo "$DATA" | grep "3\." | grep -Po '\d{2}')
   CPU1_TEMPERATURE=$(echo $CPU_DATA | awk '{print $1;}')
-  if $IS_CPU2_TEMPERATURE_SENSOR_PRESENT
-  then
+  if $IS_CPU2_TEMPERATURE_SENSOR_PRESENT; then
     CPU2_TEMPERATURE=$(echo $CPU_DATA | awk '{print $2;}')
   else
     CPU2_TEMPERATURE="-"
@@ -47,22 +45,21 @@ function retrieve_temperatures () {
   INLET_TEMPERATURE=$(echo "$DATA" | grep Inlet | grep -Po '\d{2}' | tail -1)
 
   # If exhaust temperature sensor is present, parse its temperature data
-  if $IS_EXHAUST_TEMPERATURE_SENSOR_PRESENT
-  then
+  if $IS_EXHAUST_TEMPERATURE_SENSOR_PRESENT; then
     EXHAUST_TEMPERATURE=$(echo "$DATA" | grep Exhaust | grep -Po '\d{2}' | tail -1)
   else
     EXHAUST_TEMPERATURE="-"
   fi
 }
 
-function enable_third_party_PCIe_card_Dell_default_cooling_response () {
+function enable_third_party_PCIe_card_Dell_default_cooling_response() {
   # We could check the current cooling response before applying but it's not very useful so let's skip the test and apply directly
-  ipmitool -I $IDRAC_LOGIN_STRING raw 0x30 0xce 0x00 0x16 0x05 0x00 0x00 0x00 0x05 0x00 0x00 0x00 0x00 > /dev/null
+  ipmitool -I $IDRAC_LOGIN_STRING raw 0x30 0xce 0x00 0x16 0x05 0x00 0x00 0x00 0x05 0x00 0x00 0x00 0x00 >/dev/null
 }
 
-function disable_third_party_PCIe_card_Dell_default_cooling_response () {
+function disable_third_party_PCIe_card_Dell_default_cooling_response() {
   # We could check the current cooling response before applying but it's not very useful so let's skip the test and apply directly
-  ipmitool -I $IDRAC_LOGIN_STRING raw 0x30 0xce 0x00 0x16 0x05 0x00 0x00 0x00 0x05 0x00 0x01 0x00 0x00 > /dev/null
+  ipmitool -I $IDRAC_LOGIN_STRING raw 0x30 0xce 0x00 0x16 0x05 0x00 0x00 0x00 0x05 0x00 0x01 0x00 0x00 >/dev/null
 }
 
 # Returns :
@@ -83,7 +80,7 @@ function disable_third_party_PCIe_card_Dell_default_cooling_response () {
 # }
 
 # Prepare traps in case of container exit
-function gracefull_exit () {
+function gracefull_exit() {
   apply_Dell_fan_control_profile
   enable_third_party_PCIe_card_Dell_default_cooling_response
   echo "/!\ WARNING /!\ Container stopped, Dell default dynamic fan control profile applied for safety."
@@ -98,8 +95,7 @@ trap 'gracefull_exit' SIGQUIT SIGKILL SIGTERM
 # readonly DELL_FRESH_AIR_COMPLIANCE=45
 
 # Check if FAN_SPEED variable is in hexadecimal format. If not, convert it to hexadecimal
-if [[ $FAN_SPEED == 0x* ]]
-then
+if [[ $FAN_SPEED == 0x* ]]; then
   DECIMAL_FAN_SPEED=$(printf '%d' $FAN_SPEED)
   HEXADECIMAL_FAN_SPEED=$FAN_SPEED
 else
@@ -111,8 +107,7 @@ fi
 echo "iDRAC/IPMI host: $IDRAC_HOST"
 
 # Check if the iDRAC host is set to 'local' or not then set the IDRAC_LOGIN_STRING accordingly
-if [[ $IDRAC_HOST == "local" ]]
-then
+if [[ $IDRAC_HOST == "local" ]]; then
   # Check that the Docker host IPMI device (the iDRAC) has been exposed to the Docker container
   if [ ! -e "/dev/ipmi0" ] && [ ! -e "/dev/ipmi/0" ] && [ ! -e "/dev/ipmidev/0" ]; then
     echo "/!\ Could not open device at /dev/ipmi0 or /dev/ipmi/0 or /dev/ipmidev/0, check that you added the device to your Docker container or stop using local mode. Exiting." >&2
@@ -141,19 +136,16 @@ IS_DELL_FAN_CONTROL_PROFILE_APPLIED=true
 IS_EXHAUST_TEMPERATURE_SENSOR_PRESENT=true
 IS_CPU2_TEMPERATURE_SENSOR_PRESENT=true
 retrieve_temperatures $IS_EXHAUST_TEMPERATURE_SENSOR_PRESENT $IS_CPU2_TEMPERATURE_SENSOR_PRESENT
-if [ -z "$EXHAUST_TEMPERATURE" ]
-then
+if [ -z "$EXHAUST_TEMPERATURE" ]; then
   echo "No exhaust temperature sensor detected."
   IS_EXHAUST_TEMPERATURE_SENSOR_PRESENT=false
 fi
-if [ -z "$CPU2_TEMPERATURE" ]
-then
+if [ -z "$CPU2_TEMPERATURE" ]; then
   echo "No CPU2 temperature sensor detected."
   IS_CPU2_TEMPERATURE_SENSOR_PRESENT=false
 fi
 # Output new line to beautify output if one of the previous conditions have echoed
-if ! $IS_EXHAUST_TEMPERATURE_SENSOR_PRESENT || ! $IS_CPU2_TEMPERATURE_SENSOR_PRESENT
-then
+if ! $IS_EXHAUST_TEMPERATURE_SENSOR_PRESENT || ! $IS_CPU2_TEMPERATURE_SENSOR_PRESENT; then
   echo ""
 fi
 
@@ -166,84 +158,74 @@ while true; do
   retrieve_temperatures $IS_EXHAUST_TEMPERATURE_SENSOR_PRESENT $IS_CPU2_TEMPERATURE_SENSOR_PRESENT
 
   # Define functions to check if CPU 1 and CPU 2 temperatures are above the threshold
-  function CPU1_OVERHEAT () { [ $CPU1_TEMPERATURE -gt $CPU_TEMPERATURE_THRESHOLD ]; }
-  if $IS_CPU2_TEMPERATURE_SENSOR_PRESENT
-  then
-    function CPU2_OVERHEAT () { [ $CPU2_TEMPERATURE -gt $CPU_TEMPERATURE_THRESHOLD ]; }
+  function CPU1_OVERHEAT() { [ $CPU1_TEMPERATURE -gt $CPU_TEMPERATURE_THRESHOLD ]; }
+  if $IS_CPU2_TEMPERATURE_SENSOR_PRESENT; then
+    function CPU2_OVERHEAT() { [ $CPU2_TEMPERATURE -gt $CPU_TEMPERATURE_THRESHOLD ]; }
   fi
 
   # Initialize a variable to store the comments displayed when the fan control profile changed
   COMMENT=" -"
   # Check if CPU 1 is overheating then apply Dell default dynamic fan control profile if true
-  if CPU1_OVERHEAT
-  then
+  if CPU1_OVERHEAT; then
     apply_Dell_fan_control_profile
 
-    if ! $IS_DELL_FAN_CONTROL_PROFILE_APPLIED
-    then
+    if ! $IS_DELL_FAN_CONTROL_PROFILE_APPLIED; then
       IS_DELL_FAN_CONTROL_PROFILE_APPLIED=true
 
       # If CPU 2 temperature sensor is present, check if it is overheating too.
       # Do not apply Dell default dynamic fan control profile as it has already been applied before
-      if $IS_CPU2_TEMPERATURE_SENSOR_PRESENT && CPU2_OVERHEAT
-      then
+      if $IS_CPU2_TEMPERATURE_SENSOR_PRESENT && CPU2_OVERHEAT; then
         COMMENT="CPU 1 and CPU 2 temperatures are too high, Dell default dynamic fan control profile applied for safety"
       else
         COMMENT="CPU 1 temperature is too high, Dell default dynamic fan control profile applied for safety"
       fi
     fi
   # If CPU 2 temperature sensor is present, check if it is overheating then apply Dell default dynamic fan control profile if true
-  elif $IS_CPU2_TEMPERATURE_SENSOR_PRESENT && CPU2_OVERHEAT
-  then
+  elif $IS_CPU2_TEMPERATURE_SENSOR_PRESENT && CPU2_OVERHEAT; then
     apply_Dell_fan_control_profile
 
-    if ! $IS_DELL_FAN_CONTROL_PROFILE_APPLIED
-    then
+    if ! $IS_DELL_FAN_CONTROL_PROFILE_APPLIED; then
       IS_DELL_FAN_CONTROL_PROFILE_APPLIED=true
       COMMENT="CPU 2 temperature is too high, Dell default dynamic fan control profile applied for safety"
     fi
   else
     apply_user_fan_control_profile
-    retrieve_temperatures $IS_EXHAUST_TEMPERATURE_SENSOR_PRESENT $IS_CPU2_TEMPERATURE_SENSOR_PRESENT
-
-    if [$CPU1_TEMPERATURE -le 45]; then
-      #ipmitool -I $IDRAC_LOGIN_STRING raw 0x30 0x30 0x01 0x00 >/dev/null
-      #ipmitool -I $IDRAC_LOGIN_STRING raw 0x30 0x30 0x02 0xff 0x0F >/dev/null
-      #CURRENT_FAN_CONTROL_PROFILE="User static fan control profile (15%)"
-      COMMENT="Test (15%)"
-    elif [$CPU1_TEMPERATURE -gt 45 -a $CPU1_TEMPERATURE -le 50]; then
-      #ipmitool -I $IDRAC_LOGIN_STRING raw 0x30 0x30 0x01 0x00 >/dev/null
-      #ipmitool -I $IDRAC_LOGIN_STRING raw 0x30 0x30 0x02 0xff 0x16 >/dev/null
-      #CURRENT_FAN_CONTROL_PROFILE="User static fan control profile (22%)"
-      COMMENT="Test (22%)"
-    elif [$CPU1_TEMPERATURE -gt 50 -a $CPU1_TEMPERATURE -le 55]; then
-      #ipmitool -I $IDRAC_LOGIN_STRING raw 0x30 0x30 0x01 0x00 >/dev/null
-      #ipmitool -I $IDRAC_LOGIN_STRING raw 0x30 0x30 0x02 0xff 0x20 >/dev/null
-      #CURRENT_FAN_CONTROL_PROFILE="User static fan control profile (32%)"
-      COMMENT="Test (32%)"
-    elif [$CPU1_TEMPERATURE -gt 55 -a $CPU1_TEMPERATURE -le 60]; then
-      #ipmitool -I $IDRAC_LOGIN_STRING raw 0x30 0x30 0x01 0x00 >/dev/null
-      #ipmitool -I $IDRAC_LOGIN_STRING raw 0x30 0x30 0x02 0xff 0x2E >/dev/null
-      #CURRENT_FAN_CONTROL_PROFILE="User static fan control profile (46%)"
-      COMMENT="Test (46%)"
-    else
-      #ipmitool -I $IDRAC_LOGIN_STRING raw 0x30 0x30 0x01 0x00 >/dev/null
-      #ipmitool -I $IDRAC_LOGIN_STRING raw 0x30 0x30 0x02 0xff 0x44 >/dev/null
-      #CURRENT_FAN_CONTROL_PROFILE="User static fan control profile (68%)"
-      COMMENT="Test (68%)"
-    fi
-
     # Check if user fan control profile is applied then apply it if not
     if $IS_DELL_FAN_CONTROL_PROFILE_APPLIED; then
       IS_DELL_FAN_CONTROL_PROFILE_APPLIED=false
       COMMENT="CPU temperature decreased and is now OK (<= $CPU_TEMPERATURE_THRESHOLD°C), user's fan control profile applied."
     fi
   fi
-
+  #retrieve_temperatures $IS_EXHAUST_TEMPERATURE_SENSOR_PRESENT $IS_CPU2_TEMPERATURE_SENSOR_PRESENT
+  if [$CPU1_TEMPERATURE -le 45]; then
+    #ipmitool -I $IDRAC_LOGIN_STRING raw 0x30 0x30 0x01 0x00 >/dev/null
+    #ipmitool -I $IDRAC_LOGIN_STRING raw 0x30 0x30 0x02 0xff 0x0F >/dev/null
+    #CURRENT_FAN_CONTROL_PROFILE="User static fan control profile (15%)"
+    COMMENT="Test (15%)"
+  elif [$CPU1_TEMPERATURE -gt 45 -a $CPU1_TEMPERATURE -le 50]; then
+    #ipmitool -I $IDRAC_LOGIN_STRING raw 0x30 0x30 0x01 0x00 >/dev/null
+    #ipmitool -I $IDRAC_LOGIN_STRING raw 0x30 0x30 0x02 0xff 0x16 >/dev/null
+    #CURRENT_FAN_CONTROL_PROFILE="User static fan control profile (22%)"
+    COMMENT="Test (22%)"
+  elif [$CPU1_TEMPERATURE -gt 50 -a $CPU1_TEMPERATURE -le 55]; then
+    #ipmitool -I $IDRAC_LOGIN_STRING raw 0x30 0x30 0x01 0x00 >/dev/null
+    #ipmitool -I $IDRAC_LOGIN_STRING raw 0x30 0x30 0x02 0xff 0x20 >/dev/null
+    #CURRENT_FAN_CONTROL_PROFILE="User static fan control profile (32%)"
+    COMMENT="Test (32%)"
+  elif [$CPU1_TEMPERATURE -gt 55 -a $CPU1_TEMPERATURE -le 60]; then
+    #ipmitool -I $IDRAC_LOGIN_STRING raw 0x30 0x30 0x01 0x00 >/dev/null
+    #ipmitool -I $IDRAC_LOGIN_STRING raw 0x30 0x30 0x02 0xff 0x2E >/dev/null
+    #CURRENT_FAN_CONTROL_PROFILE="User static fan control profile (46%)"
+    COMMENT="Test (46%)"
+  else
+    #ipmitool -I $IDRAC_LOGIN_STRING raw 0x30 0x30 0x01 0x00 >/dev/null
+    #ipmitool -I $IDRAC_LOGIN_STRING raw 0x30 0x30 0x02 0xff 0x44 >/dev/null
+    #CURRENT_FAN_CONTROL_PROFILE="User static fan control profile (68%)"
+    COMMENT="Test (68%)"
+  fi
   # Enable or disable, depending on the user's choice, third-party PCIe card Dell default cooling response
   # No comment will be displayed on the change of this parameter since it is not related to the temperature of any device (CPU, GPU, etc...) but only to the settings made by the user when launching this Docker container
-  if $DISABLE_THIRD_PARTY_PCIE_CARD_DELL_DEFAULT_COOLING_RESPONSE
-  then
+  if $DISABLE_THIRD_PARTY_PCIE_CARD_DELL_DEFAULT_COOLING_RESPONSE; then
     disable_third_party_PCIe_card_Dell_default_cooling_response
     THIRD_PARTY_PCIE_CARD_DELL_DEFAULT_COOLING_RESPONSE_STATUS="Disabled"
   else
@@ -252,8 +234,7 @@ while true; do
   fi
 
   # Print temperatures, active fan control profile and comment if any change happened during last time interval
-  if [ $i -eq $TABLE_HEADER_PRINT_INTERVAL ]
-  then
+  if [ $i -eq $TABLE_HEADER_PRINT_INTERVAL ]; then
     echo "                     ------- Temperatures -------"
     echo "    Date & time      Inlet  CPU 1  CPU 2  Exhaust          Active fan speed profile          Third-party PCIe card Dell default cooling response  Comment"
     i=0
